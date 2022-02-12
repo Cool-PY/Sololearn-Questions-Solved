3c2b9aa6a0c20ea2262c7645c71e0be3baf3dd28
Cool__Div__ <93518942+Cool-Py@users.noreply.github.com>
2022-02-12 14:47:56 +0530

    Update Fruit Bowl.py

--- Fruit Bowl.py
+++ Fruit Bowl.py
@@ -1,1 +1,5 @@
-print(24)
+fruits = int(input()) # Taking user input as an integer
+
+apples = fruits / 2 # Taking out the number of apples in the fruit bowl
+pie = apples / 3 # Taking out the number of pies you can make from the apples
+print(int(pie)) # Printing the numbers of pie in an integer
